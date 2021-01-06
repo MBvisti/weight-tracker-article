ec06ae6273656f4ca625a67ae8cda2ac320f68bd
Morten Vistisen <mv@legalmonster.com>
2021-01-06 20:18:19 -0300

    added new storage and new server

--- pkg/app/server.go
+++ pkg/app/server.go
@@ -1,1 +1,17 @@
 package app
+
+import "weight-tracker/pkg/api"
+
+type Server struct {
+	router        *gin.Engine
+	userService   api.UserService
+	weightService api.WeightService
+}
+
+func NewServer(router *gin.Engine, userService api.UserService, weightService api.WeightService) *Server {
+	return &Server{
+		router:        router,
+		userService:   userService,
+		weightService: weightService,
+	}
+}
--- pkg/repository/storage.go
+++ pkg/repository/storage.go
@@ -1,1 +1,14 @@
 package repository
+
+import "database/sql"
+
+type Storage interface {
+}
+
+type storage struct {
+	db *sql.DB
+}
+
+func NewStorage(db *sql.DB) Storage {
+	return &storage{db: db}
+}
