e7b3500c7d9a871be9d524d30e229f0b36720d3d
Morten Vistisen <mv@legalmonster.com>
2021-01-06 20:17:44 -0300

    name change and formatting

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"weight-tracker/pkg/api"
+	"weight-tracker/pkg/app"
+	"weight-tracker/pkg/repository"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "this is the startup error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+// func run will be responsible for setting up db connections, routers etc
+func run() error {
+	// I'm used to working with postgres feel free to use any db you would like
+	// I'm not going to cover how to create a database here but create a database
+	// and call it something along the lines of "weight tracker"
+	connectionString := "insert-connection-string-here"
+
+	// setup database connection
+	db, err := setupDatabase(connectionString)
+	if err != nil {
+		return err
+	}
+
+	// create storage dependency
+	storage := repository.NewStorage(db)
+
+	// create router dependecy
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	// create user service
+	userService := api.NewUserService(storage)
+
+	// create weight service
+	weightService := api.NewWeightService(storage)
+
+	server := app.NewServer(router, userService, weightService)
+
+	// start the server
+	err = server.Run()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setupDatabase(connString string) (*sql.DB, error) {
+	// change "postgres" for whatever supported database you want to use
+	db, err := sql.Open("postgres", connString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// ping the DB to ensure that it is connected
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
