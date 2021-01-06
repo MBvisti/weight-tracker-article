32bec25b518d72f85098718f8c58163099a05e26
Morten Vistisen <mv@legalmonster.com>
2021-01-06 20:18:03 -0300

    added the base of the two services

--- pkg/api/user.go
+++ pkg/api/user.go
@@ -1,1 +1,17 @@
 package api
+
+// UserService contains the methods of the user service
+type UserService interface {
+}
+
+// User repository is what lets our service do db operations without knowing anything about the implementation
+type UserRepository interface {
+}
+
+type userService struct {
+	storage UserRepository
+}
+
+func NewUserService(userRepo UserRepository) UserService {
+	return &userService{storage: userRepo}
+}
--- pkg/api/weight.go
+++ pkg/api/weight.go
@@ -1,1 +1,18 @@
 package api
+
+// WeightService contains the methods of the user service
+type WeightService interface {
+}
+
+// WeightRepository is what lets our service do db operations without knowing anything about the implementation
+type WeightRepository interface {
+}
+
+// weightService contains the actual implementations of the methods defined in WeightService
+type weightService struct {
+	storage WeightRepository
+}
+
+func NewWeightService(weightRepo WeightRepository) WeightService {
+	return &weightService{storage: weightRepo}
+}
