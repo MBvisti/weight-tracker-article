181b9496a4bab022baba24a9d00cff9822dc674f
Morten Vistisen <mv@legalmonster.com>
2021-01-07 17:59:02 -0300

    last changes to make project run - with a status endpoint

--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -1,1 +1,23 @@
 package app
+
+func (s *Server) Routes() *gin.Engine {
+	router := s.router
+
+	// group all routes under /v1/api
+	v1 := router.Group("/v1/api")
+	{
+		// prefix the user routes
+		user := v1.Group("/user")
+		{
+
+		}
+
+		// prefix the weight routes
+		weight := v1.Group("/weight")
+		{
+
+		}
+	}
+
+	return router
+}
--- pkg/app/server.go
+++ pkg/app/server.go
@@ -1,6 +1,9 @@
 package app
 
-import "weight-tracker/pkg/api"
+import (
+	"log"
+	"weight-tracker/pkg/api"
+)
 
 type Server struct {
 	router        *gin.Engine
@@ -14,4 +17,19 @@ func NewServer(router *gin.Engine, userService api.UserService, weightService ap
 		userService:   userService,
 		weightService: weightService,
 	}
+}
+
+func (s *Server) Run() error {
+	// run function that initializes the routes
+	r := s.Routes()
+
+	// run the server through the router
+	err := r.Run()
+
+	if err != nil {
+		log.Printf("Server - there was an error calling Run on router: %v", err)
+		return err
+	}
+
+	return nil
 }
