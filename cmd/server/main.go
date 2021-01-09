21aa838fa836b0dc6777f64731a4abb33e7f58f4
Morten Vistisen <mv@legalmonster.com>
2021-01-09 15:08:34 -0300

    connection string to db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,7 +23,7 @@ func run() error {
 	// I'm used to working with postgres feel free to use any db you would like
 	// I'm not going to cover how to create a database here but create a database
 	// and call it something along the lines of "weight tracker"
-	connectionString := "insert-connection-string-here"
+	connectionString := "postgres://postgres:postgres@localhost/weight_tracker?sslmode=disable"
 
 	// setup database connection
 	db, err := setupDatabase(connectionString)
