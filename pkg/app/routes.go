3468899e0263054dcb1d786e628c3cdeb4a824b6
Morten Vistisen <mv@legalmonster.com>
2021-01-09 15:08:18 -0300

    empty routes for user and weight for now

--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -8,16 +8,17 @@ func (s *Server) Routes() *gin.Engine {
 	// group all routes under /v1/api
 	v1 := router.Group("/v1/api")
 	{
+		v1.GET("/status", s.ApiStatus())
 		// prefix the user routes
 		user := v1.Group("/user")
 		{
-
+			user.GET("")
 		}
 
 		// prefix the weight routes
 		weight := v1.Group("/weight")
 		{
-
+			weight.GET("")
 		}
 	}
 
