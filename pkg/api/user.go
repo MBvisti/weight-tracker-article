4a71b0fbea8c49a9f15b2076e5b5c60a90ac58b5
Morten Vistisen <mv@legalmonster.com>
2021-01-10 17:51:45 -0300

    adding user methods on service

--- pkg/api/user.go
+++ pkg/api/user.go
@@ -1,11 +1,20 @@
 package api
 
+import (
+	"errors"
+	"strings"
+)
+
 // UserService contains the methods of the user service
 type UserService interface {
+	New(user NewUserRequest) error
+	UpdateActivityLevel(res UpdateActivityLevelRequest) error
 }
 
 // User repository is what lets our service do db operations without knowing anything about the implementation
 type UserRepository interface {
+	CreateUser(NewUserRequest) error
+	UpdateActivityLevel(UpdateActivityLevelRequest) error
 }
 
 type userService struct {
@@ -14,4 +23,42 @@ type userService struct {
 
 func NewUserService(userRepo UserRepository) UserService {
 	return &userService{storage: userRepo}
+}
+
+// TODO: note that we are not checking if the user already exists - we probably should not the article will be too long
+func (u *userService) New(user NewUserRequest) error {
+	if user.Email == "" {
+		return errors.New("user service - email required")
+	}
+
+	if user.Name == "" {
+		return errors.New("user service - name required")
+	}
+
+	// do some basic normalization
+	user.Name = strings.ToLower(user.Name)
+	user.Email = strings.TrimSpace(user.Email)
+
+	err := u.storage.CreateUser(user)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TODO: activity level is loosely defined here - should be a database set value
+func (u *userService) UpdateActivityLevel(res UpdateActivityLevelRequest) error {
+	if res.Email == "" {
+		return errors.New("user service - no email provided")
+	}
+
+	err := u.storage.UpdateActivityLevel(res)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
