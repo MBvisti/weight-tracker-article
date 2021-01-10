3a309e2767061a312c3ffb6ce5927f9e5664079f
Morten Vistisen <mv@legalmonster.com>
2021-01-10 17:52:28 -0300

    adding create user handler

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -1,7 +1,9 @@
 package app
 
 import (
+	"log"
 	"net/http"
+	"weight-tracker/pkg/api"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +15,36 @@ func (s *Server) ApiStatus() gin.HandlerFunc {
 		response := map[string]string{
 			"status": "success",
 			"data":   "weight tracker API running smoothly",
+		}
+
+		c.JSON(http.StatusOK, response)
+	}
+}
+
+func (s *Server) CreateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Content-Type", "application/json")
+
+		var newUser api.NewUserRequest
+		err := c.ShouldBindJSON(&newUser)
+
+		if err != nil {
+			log.Printf("handler error: %v", err)
+			c.JSON(http.StatusBadRequest, nil)
+			return
+		}
+
+		err = s.userService.New(newUser)
+
+		if err != nil {
+			log.Printf("service error: %v", err)
+			c.JSON(http.StatusInternalServerError, nil)
+			return
+		}
+
+		response := map[string]string{
+			"status": "success",
+			"data":   "new user created",
 		}
 
 		c.JSON(http.StatusOK, response)
