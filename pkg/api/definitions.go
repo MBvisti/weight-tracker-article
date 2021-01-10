4a9ef7b4a97842ffe7160c171cdd0de45b2df433
Morten Vistisen <mv@legalmonster.com>
2021-01-10 17:51:34 -0300

    user definitions

--- pkg/api/definitions.go
+++ pkg/api/definitions.go
@@ -1,1 +1,15 @@
 package api
+
+type NewUserRequest struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+	Height int `json:"height"`
+	Sex string `json:"sex"`
+	ActivityLevel int `json:"activity_level"`
+	Email string `json:"email"`
+}
+
+type UpdateActivityLevelRequest struct {
+	Email string `json:"email"`
+	ActivityLevel int `json:"activity_level"`
+}
