544d44e991d6c63eb63585db731fe605ec9e4fa6
Morten Vistisen <mv@legalmonster.com>
2021-01-10 18:52:44 -0300

    adding weight handler

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -50,3 +50,33 @@ func (s *Server) CreateUser() gin.HandlerFunc {
 		c.JSON(http.StatusOK, response)
 	}
 }
+
+func (s *Server) CreateWeightEntry() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Content-Type", "application/json")
+
+		var newWeight api.NewWeightRequest
+		err := c.ShouldBindJSON(&newWeight)
+
+		if err != nil {
+			log.Printf("handler error: %v", err)
+			c.JSON(http.StatusBadRequest, nil)
+			return
+		}
+
+		err = s.weightService.New(newWeight)
+
+		if err != nil {
+			log.Printf("service error: %v", err)
+			c.JSON(http.StatusInternalServerError, nil)
+			return
+		}
+
+		response := map[string]string{
+			"status": "success",
+			"data":   "new user created",
+		}
+
+		c.JSON(http.StatusOK, response)
+	}
+}
